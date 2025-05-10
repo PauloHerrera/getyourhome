d8f393cc755348fce8b918cdd791c193a1a058ef
Paulo Herrera <pauloherreralopes@gmail.com>
2025-05-10 17:48:11 -0300

    -chore: add amenity-categories to seed script

--- supabase/supabase/seed/initial_data.sql
+++ supabase/supabase/seed/initial_data.sql
@@ -1,3 +1,4 @@
+
 -- Insert default property types
 INSERT INTO property_types (name, description) VALUES 
   ('Apartamento', 'Unidade residencial em um edifício com várias unidades'),
@@ -14,9 +15,24 @@ INSERT INTO property_types (name, description) VALUES
   ('Ponto Comercial', 'Ponto Comercial'),
   ('Outros', 'Outros');
 
-INSERT INTO property_amenities (name) VALUES 
-  ('Área de lazer'),
-  ('Piscina'),
-  ('Churrasqueira'),
-  ('Garagem'),
-  ('Outros');
+-- Insert default property amenity categories with UUIDs
+INSERT INTO amenity_categories (id, name) VALUES 
+  ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11', 'Condomínio'),
+  ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a12', 'Comercial'), 
+  ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a13', 'Infraestrutura'),
+  ('a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a14', 'Outros');
+
+-- Insert default property amenities
+INSERT INTO property_amenities (name, amenity_category_id) VALUES 
+  ('Área de lazer', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11'),
+  ('Piscina', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11'),
+  ('Churrasqueira', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11'),
+  ('Garagem', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11'),
+  ('Varanda Gourmet', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a13'),
+  ('Gás encanado', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a13'),
+  ('Área de serviço', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a13'),
+  ('Próximo a praia', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a14'),
+  ('Próximo a parque', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a14'),
+  ('Próximo a hospital', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a14'),
+  ('Próximo a escola', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a14'),
+  ('Próximo a shopping', 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a14');
